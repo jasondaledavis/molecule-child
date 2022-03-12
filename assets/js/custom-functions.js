08c4df36078ba5a0d5e63fc3d66dbc9c037247c2
Jason <jason@Jasons-MBP.attlocal.net>
2022-03-12 12:34:14 -0500

    v2.5

--- assets/js/custom-functions.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(e){"use strict";e(function(){var o=e(".header-top");e(window).on("scroll",function(){e(window).scrollTop()>=50?o.removeClass("header-top").addClass("header-top-dark"):o.removeClass("header-top-dark").addClass("header-top")})})});
